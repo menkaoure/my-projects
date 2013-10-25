066673f2c1330f9001b53c2c2bc8fcd83cc5ccef
menkaoure <lucky7luke@gmail.com>
2013-10-25 11:22:35 +0200

    modif in app and views's files

--- JS-Codes/Node_Tuto/app.js
+++ JS-Codes/Node_Tuto/app.js
@@ -5,10 +5,15 @@ var app = express();
 app.get('/', function(req, res) {
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Vous etes a l\'acceuil ');
-});
+})
+.get('/todo', function(req, res) {
+	var tasksList = ['task0', 'task1', 'task2'];
 
-app.use(function(req, res, next) {
+	res.render('todolist.ejs', {title: 'Todo List', tasks: tasksList});
+})
+.use(function(req, res, next) {
 	res.setHeader('Content-Type', 'text/plain');
 	res.send(404, 'Page introuvable');
-})
+});
+
 app.listen(4000);
